daa90dcd7977ab190a17580dd633c1f94187f694
zackenstien <trimorphyt@gmail.com>
2020-08-06 18:42:08 -0400

    :art: Use tabs instead of spaces.

--- array/array_length.js
+++ array/array_length.js
@@ -1,13 +1,13 @@
 module.exports = function array_length(arr) {
-    if (typeof arr == 'object') {
-        if (Array.isArray(arr))
-            return arr.length;
-        else {
-            console.error(`Expected array; got ${typeof arr}`);
-            process.exit(1);
-        }
-    } else {
-        console.error(`Expected array; got ${typeof arr}`);
-        process.exit(1);
-    }
-}
+	if (typeof arr == 'object') {
+		if (Array.isArray(arr))
+			return arr.length;
+		else {
+			console.error(`Expected array; got ${typeof arr}`);
+			process.exit(1);
+		}
+	} else {
+		console.error(`Expected array; got ${typeof arr}`);
+		process.exit(1);
+	}
+};
