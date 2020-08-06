ed8e021f4e186517db6db54d80754c98a6394608
zackenstien <trimorphyt@gmail.com>
2020-08-06 18:42:58 -0400

    :art: Use tabs.

--- array/array_values.js
+++ array/array_values.js
@@ -1,26 +1,26 @@
 module.exports = function array_values(arr) {
-    if (typeof arr == 'object') {
-        if (Array.isArray(arr)) {
-            var outputArray = [];
+	if (typeof arr == 'object') {
+		if (Array.isArray(arr)) {
+			var outputArray = [];
             
-            function search(array) {
-                array.forEach(item => {
-                    if (Array.isArray(item)) {
-                        search(item);
-                    } else {
-                        outputArray.push(item);
-                    }
-                });
-            }
+			function search(array) {
+				array.forEach(item => {
+					if (Array.isArray(item)) {
+						search(item);
+					} else {
+						outputArray.push(item);
+					}
+				});
+			}
 
-            search(arr);
-            return outputArray;
-        } else {
-            console.error(`Expected array; got ${typeof arr}`);
-            process.exit(1);
-        }
-    } else {
-        console.error(`Expected array; got ${typeof arr}`);
-        process.exit(1);
-    }
-}
+			search(arr);
+			return outputArray;
+		} else {
+			console.error(`Expected array; got ${typeof arr}`);
+			process.exit(1);
+		}
+	} else {
+		console.error(`Expected array; got ${typeof arr}`);
+		process.exit(1);
+	}
+};
